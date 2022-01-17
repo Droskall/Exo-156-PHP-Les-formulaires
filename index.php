0b4ee190daf5551a21ca395d9a83322341e4340e
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-01-17 15:44:40 +0100

    fini

---
+++ index.php
@@ -0,0 +1,12 @@
+
+
+
+<form method="post" action="forms.php">
+    <div>
+        <label for="name">Nom</label>
+        <input type="text" name="nom" id="name" required>
+    </div>
+    <div>
+        <input type="submit" value="Envoyer">
+    </div>
+</form>
